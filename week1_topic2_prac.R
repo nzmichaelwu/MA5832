5ccad80ebd6d37ee495891ec12587f4ee7e6ba0b
Michael Wu <nzmichaelwu@hotmail.com>
2021-09-20 22:03:52 +1000

    commiting A1.rmd, and the draft of A2.rmd.

--- week1_topic2_prac.R
+++ week1_topic2_prac.R
@@ -11,3 +11,12 @@ cluster_heart <- heart_data %>%
 str(cluster_heart)
 
 heart_data$Disease <- as.factor(cluster_heart$cluster)
+
+# logistic regression
+glm.fit <- glm(V10 ~ sbp + ldl + obesity + famhist, data = heart_data, family = binomial())
+
+summary (glm.fit) # to observe model summaries
+glm.probs <- predict(glm.fit,type = "response") # to obtain the probability that individual “i” has the disease.
+glm.pred <- ifelse(glm.probs > 0.5, "Up", "Down") # for classification based on the model.
+
+table(glm.probs, heart_data$V10)
